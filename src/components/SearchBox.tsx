e521a96971fc57d6305fbf191d9191ed07a4653b
m.dolati <marziye.dl@gmail.com>
2023-08-21 19:12:15 +0330

    fix: 🐛 trigger first component initial and avoid api call

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,6 +1,6 @@
 import styles from "@/assets/styles/page.module.css";
 
-import { ChangeEvent, FC, memo, useEffect, useState } from "react";
+import { ChangeEvent, FC, memo, useEffect, useRef, useState } from "react";
 import { t } from "i18next";
 
 import { useDebounce } from "@/hooks/useDebounce";
@@ -12,6 +12,7 @@ interface Props {
 export const SearchBox: FC<Props> = memo(({ onChange }) => {
   const [searchValue, setSearchValue] = useState("");
   const debouncedSearchTerm = useDebounce(searchValue, 300);
+  const isFirstRun = useRef(true);
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -20,6 +21,10 @@ export const SearchBox: FC<Props> = memo(({ onChange }) => {
   };
 
   useEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      return;
+    }
     onChange?.(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
 
@@ -27,8 +32,8 @@ export const SearchBox: FC<Props> = memo(({ onChange }) => {
     <section className="m-1 w-100">
       <input
         className={styles.input}
-        type="text"
-        placeholder={t('search')}
+        type="search"
+        placeholder={t("search")}
         onChange={handleInput}
       />
     </section>
